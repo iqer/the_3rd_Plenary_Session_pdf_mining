0f116e5d3be3225177cea0f77db216fb348579c1
ArthurChen <771562385@qq.com>
2024-07-25 10:25:49 +0800

    style: unify py code style

--- pdf_extract.py
+++ pdf_extract.py
@@ -3,7 +3,7 @@ import pathlib
 import pdfplumber
 
 dir_path = pathlib.Path(__file__).parent
-file_name = 'china_gov_decision.pdf'
+file_name = "china_gov_decision.pdf"
 file_path = dir_path.joinpath(file_name)
 
 text_list = []
@@ -16,12 +16,13 @@ with pdfplumber.open(file_path) as pdf:
             index = cur_text.find("要要求求")
             cur_text = cur_text[index + 5:]
         cur_text = cur_text.removeprefix(
-            '7/24/24, 12:13 PM 中共中央关于进⼀步全⾯深化改⾰ 推进中国式现代化的决定_中央有关⽂件_中国政府⽹\n')
-        index = cur_text.find('https://www.gov.cn/zhengce/202407/content_6963770.htm')
+            "7/24/24, 12:13 PM 中共中央关于进⼀步全⾯深化改⾰ 推进中国式现代化的决定_中央有关⽂件_中国政府⽹\n"
+        )
+        index = cur_text.find("https://www.gov.cn/zhengce/202407/content_6963770.htm")
         cur_text = cur_text[:index]
         text_list.append(cur_text)
 
-article = ''.join(text_list)
+article = "".join(text_list)
 
-with open('article.text', 'w') as f:
+with open("article.text", "w") as f:
     f.write(article)
